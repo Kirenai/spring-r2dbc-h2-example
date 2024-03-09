1b25d68b4824e7d064d8a1c6a963929772e76443
Kirenai <pool_dota2@hotmail.com>
2024-03-08 22:41:02 -0500

    Init commit

---
+++ src/main/java/me/kirenai/re/r2dbch2/SpringR2dbcH2ExampleApplication.java
@@ -0,0 +1,13 @@
+package me.kirenai.re.r2dbch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringR2dbcH2ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringR2dbcH2ExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/kirenai/re/r2dbch2/SpringR2dbcH2ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kirenai.re.r2dbch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringR2dbcH2ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
